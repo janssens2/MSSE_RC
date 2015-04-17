90b22b9834fa52095d3749cf7f9152736c4ad36c
Mike Weispfenning <weis0292@umn.edu>
2015-04-17 12:32:40 -0500

    Moved some code to the appropriate place.  Still trying to get our solution fully set up.

--- RC_Car/RC_Car_Controller/main.c
+++ RC_Car/RC_Car_Controller/main.c
@@ -1,30 +1,64 @@
-/* RC_Car_Controller - an application for the Pololu Orangutan SVP
+/*
+ * main.c
  *
- * This application uses the Pololu AVR C/C++ Library.  For help, see:
- * -User's guide: http://www.pololu.com/docs/0J20
- * -Command reference: http://www.pololu.com/docs/0J18
- *
- * Created: 4/17/2015 11:44:23 AM
- *  Author: janssens
+ * Created: 4/3/2015 10:21:24 AM
+ * Author: Mike Weispfenning
  */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
 #include <pololu/orangutan.h>
 
+#include "headers/motion_control.h"
+#include "headers/timers.h"
+
+void print_motion_control_info();
+void release_print_task();
+
+volatile bool g_release_print_task = false;
+
 int main()
 {
-	play_from_program_space(PSTR(">g32>>c32"));  // Play welcoming notes.
+	clear();
+	lcd_init_printf();
 
-	while(1)
+	timer_three_set_to_one_hundred_milliseconds(&release_print_task);
+
+	while(true)
 	{
-		// Get battery voltage (in mV) from the auxiliary processor
-		// and print it on the the LCD.
-		clear();
-		print_long(read_battery_millivolts_svp());
-
-		red_led(1);     // Turn on the red LED.
-		delay_ms(200);  // Wait for 200 ms.
-
-		red_led(0);     // Turn off the red LED.
-		delay_ms(200);  // Wait for 200 ms.
+		if (g_release_print_task)
+		{
+			g_release_print_task = false;
+			print_motion_control_info();
+		}
 	}
+}
+
+void release_print_task()
+{
+	g_release_print_task = true;
+}
+
+void print_motion_control_info()
+{
+	clear();
+
+	uint8_t input_method = motion_control_get_input_method();
+	if (input_method == MOTION_CONTROL_JOYSTICK)
+	{
+		printf("[JS]  ACCEL");
+	}
+	else if (input_method == MOTION_CONTROL_ACCELEROMETER)
+	{
+		printf(" JS  [ACCEL]");
+	}
+
+	lcd_goto_xy(0, 1);
+
+	//printf("X:%+4d Y:%+4d", motion_control_get_x_input_as_percentage(), motion_control_get_y_input_as_percentage());
+	printf("X:%+4d Y:%+4d", motion_control_get_x_input(), motion_control_get_y_input());
+
+	set_m1_speed((motion_control_get_y_input_as_percentage() * 0xFF) / 100);
 }
