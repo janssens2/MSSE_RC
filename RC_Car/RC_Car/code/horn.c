f1902d832ab3f2b16590cae8cbbb6cd6ccb85794
Mike Weispfenning <weis0292@umn.edu>
2015-04-25 21:15:31 -0500

    Changed the horn to play from program space, which the documentation mentioned is preferrable.

--- RC_Car/RC_Car/code/horn.c
+++ RC_Car/RC_Car/code/horn.c
@@ -11,15 +11,18 @@
 
 #include <pololu/orangutan.h>
 
+const char la_cucaracha_short[] PROGMEM = "! T280 L4 V12 MS G8 G8 G8 >C4 >E8 G8 G8 G8 >C4 >E2 >C8 >C8 B8 B8 A8 A8 G4.";
+const char la_cucaracha_long[] PROGMEM = "! T280 L4 V12 MS G8 G8 G8 >C4 >E8 G8 G8 G8 >C4 >E2 >C8 >C8 B8 B8 A8 A8 G4. G8 G8 G8 B4 >D8 G8 G8 G8 B4 >D2 >G8 >A8 >G8 >F8 >E8 >D8 >C2.";
+
 void horn_honk(uint8_t version)
 {
 	// La Cucaracha!!!
 	if (version == HORN_VERSION_SHORT)
 	{
-		play("! T280 L4 V12 MS G8 G8 G8 >C4 >E8 G8 G8 G8 >C4 >E2 >C8 >C8 B8 B8 A8 A8 G4.");
+		play_from_program_space(la_cucaracha_short);
 	}
 	else if (version == HORN_VERSION_LONG)
 	{
-		play("! T280 L4 V12 MS G8 G8 G8 >C4 >E8 G8 G8 G8 >C4 >E2 >C8 >C8 B8 B8 A8 A8 G4. G8 G8 G8 B4 >D8 G8 G8 G8 B4 >D2 >G8 >A8 >G8 >F8 >E8 >D8 >C2.");
+		play_from_program_space(la_cucaracha_long);
 	}
 }
